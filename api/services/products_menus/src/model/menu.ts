69d7a2012a0976fa6bd136e42299b4a20593c3dd
EKOBE MOUKODI NICOLAS <nicolas.ekobe@gmail.com>
2021-06-17 23:47:15 +0200

    menu micro service competed

---
+++ api/services/products_menus/src/model/menu.ts
@@ -0,0 +1,76 @@
+import * as mongoose from 'mongoose';
+
+/**
+ * Menu objects allow merchants to show their available
+ * menus for sale
+ */
+interface IMenu {
+   /**
+    * Uniq indentifier
+    */
+    id: string;
+    
+    /**
+     * Name which represent the menu
+     */
+    name: string;
+    
+    /**
+     * Short text describing the menu
+     */
+    description: string;
+
+    /**
+     * Restaurant uniq indentifier reference
+     */
+    restaurant_id: number;
+
+    /**
+     * Price in euros (€)
+     */
+    price?: number;
+
+    /**
+     * Picture URI
+     */
+    //picture?: string;
+
+    /**
+     * Is the menu currently available ?
+     */
+    available: boolean;
+
+    /**
+     * Products contained on the menu
+     * TODO
+     */
+    products?: Array<object>;
+
+    
+}
+
+const MenuSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  restaurant_id: Number,
+  price: Number,
+  available: Boolean,
+  //picture: String,
+  products: {
+    step: String,
+    product_id:  mongoose.Schema.Types.ObjectId ,
+    quantity: Number,
+  },
+});
+
+
+MenuSchema.method("toJSON", function(this: any) {
+	const { __v, _id, ...object } = this.toObject();
+	object.id = _id;
+	return object;
+  });
+  
+
+const MenuModel = mongoose.model('menus', MenuSchema);
+
+export { MenuModel, IMenu }
---
+++ api/services/products_menus/src/services/menu.services.ts
@@ -0,0 +1,46 @@
+import { MenuModel, IMenu } from "../model/menu";
+import {Tags} from 'tsoa';
+
+
+export type MenuCreationParams = Omit<IMenu, "id">;
+export type MenuUpdateParams = Partial<Omit<IMenu, "id">>;
+
+export class MenusService {
+
+  public async getAll(): Promise<IMenu[]> {
+    try {
+      let items: any = await MenuModel.find({})
+      //items = items.map((item: { _id: string; description: string; available: boolean }) => { return { _id: item._id, description: item.description, available: item.available } })
+      return items;
+    } catch (err) {
+      console.error('Caught error', err)
+      return [];
+    }
+  }
+
+  public async create(menuCreationParams: MenuCreationParams): Promise<boolean> {
+
+    const item = new MenuModel(menuCreationParams)
+    console.info(menuCreationParams)
+    let success = false;
+    await item.save(function(err, menu) {
+        if (err) success = false;
+        else success = true;
+    });
+
+    return success;
+  }
+
+  public async update(id: string, menuUpdateParams: MenuUpdateParams): Promise<void> {
+    await MenuModel.findByIdAndUpdate({ _id: id }, menuUpdateParams)
+  }
+
+  public async delete(id: string): Promise<void> {
+    await MenuModel.findByIdAndRemove(id)
+  }
+
+  public async get(id: string): Promise<any> {
+    return await MenuModel.findById(id)
+  }
+
+}
