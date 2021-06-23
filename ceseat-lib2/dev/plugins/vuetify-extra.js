2c52481fc3970f510343426cd72d10a10a9a0035
Antoine67 <antoine.mohr@viacesi.fr>
2021-06-23 10:42:14 +0200

    Merge components into dev

--- ceseat-lib2/dev/plugins/vuetify-extra.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import VuetifyExtra from '@/plugin' /* point to /src/plugin.js for dev purpose
-in production replace with: import VuetifyExtra from '@menteora/vuetify-extra' and
-import '@menteora/vuetify-extra/dist/menteora.css' */
-
-Vue.use(VuetifyExtra)
