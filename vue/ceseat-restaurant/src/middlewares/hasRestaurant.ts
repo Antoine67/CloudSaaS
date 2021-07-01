34831d32b19659939b6241ef21f2ccea523ca2d8
EKOBE MOUKODI NICOLAS <nicolas.ekobe@gmail.com>
2021-07-01 15:20:27 +0200

    middleware modif token decrypt working

--- vue/ceseat-restaurant/src/middlewares/hasRestaurant.ts
+++ vue/ceseat-restaurant/src/middlewares/hasRestaurant.ts
@@ -1,12 +1,12 @@
 //import VueCookie from 'vue-cookies'
+import store from '../store'
 
 export default function hasRestaurant({ next, router } : any) {
 
     const storedUser = localStorage.getItem('ceseat_restaurant_user');
     const jwt_cookie = JSON.parse(storedUser ? storedUser : "");
-    console.log();
-      
-    if (!jwt_cookie || !jwt_cookie.restaurantId) {
+    
+    if (!jwt_cookie || !store.state.Auth.userData.restaurantId) {
       return router.push({ name: 'NoRestaurantDashboard' });
     }
     return next();
